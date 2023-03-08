ef968140016716677a06ae9c018ea849da2aa3fe
Santiago Rosas <santiagorosas.contacto@gmail.com>
2023-03-07 22:55:05 -0300

    Actividad finalizada correctamente

---
+++ src/components/mensajero/Mensajero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Mensajero = (props)=> {
+  return (
+    <>
+      <h1>{props.children}</h1>
+    </>
+  )
+}
---
+++ src/components/mensajero/mensaje/Mensaje.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Hola = (props)=> {
+
+
+  return (
+    <>
+      <h1>{props.saludo}  {props.nombre}</h1>
+    </>
+  )
+}
